f1483a7a02b6c2d2cc7aec6f390610d6b8301735
Ray <zai7lou@outlook.com>
2023-11-15 01:02:28 +0800

    update

---
+++ src/Ray.Infrastructure/Ray/DDD/DataFilterState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ray.DDD
+{
+    public class DataFilterState
+    {
+        public bool IsEnabled { get; set; }
+
+        public DataFilterState(bool isEnabled)
+        {
+            IsEnabled = isEnabled;
+        }
+    }
+}
---
+++ src/Ray.Infrastructure/Ray/DDD/IDataFilter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ray.DDD
+{
+    public interface IDataFilter<TFilter> where TFilter : class
+    {
+        IDisposable Enable();
+
+        IDisposable Disable();
+
+        bool IsEnabled { get; }
+    }
+
+    public class DataFilter<TFilter> : IDataFilter<TFilter> where TFilter : class
+    {
+        public bool IsEnabled
+        {
+            get
+            {
+                EnsureInitialized();
+                return _filter.Value.IsEnabled;
+            }
+        }
+
+        private readonly AsyncLocal<DataFilterState> _filter;
+
+        public DataFilter()
+        {
+            _filter = new AsyncLocal<DataFilterState>();
+        }
+
+        public IDisposable Enable()
+        {
+            EnsureInitialized();
+            _filter.Value.IsEnabled = true;
+
+            return new DisposeAction(() => Disable());
+        }
+
+        public IDisposable Disable()
+        {
+            EnsureInitialized();
+            _filter.Value.IsEnabled = false;
+
+            return new DisposeAction(() => Enable());
+        }
+
+        private void EnsureInitialized()
+        {
+            if (_filter.Value != null) return;
+
+            _filter.Value = new DataFilterState(true);
+        }
+    }
+
+}
